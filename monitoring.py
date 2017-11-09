6c03248e9586ce3bb6dfc4c61aa4ee355fe56f78
grau Puche <grauPuche@users.noreply.github.com>
2017-11-09 00:00:18 -0500

    Create monitoring.py

---
+++ monitoring.py
@@ -0,0 +1,42 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class Watcher:
+    DIRECTORY_TO_WATCH = "/Volumes/grau'"+"s/0-proj/_faces/narciatio/v4/output/crop/"
+
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print "Error"
+
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        elif event.event_type == 'created':
+            # Take any action here when a file is first created.
+            print "Received created event - %s." % event.src_path
+			# ser.write("1")
+
+
+
+if __name__ == '__main__':
+    w = Watcher()
+    w.run()
